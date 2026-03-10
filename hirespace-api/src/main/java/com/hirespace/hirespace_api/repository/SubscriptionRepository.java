fa610a72de810eafe8e15b98658fd7eb949c5f43
Ishaq-879 <ishaqmahmood2006@gmail.com>
2026-03-10 01:35:24 +0000

    work

---
+++ hirespace-api/src/main/java/com/hirespace/hirespace_api/repository/SubscriptionRepository.java
@@ -0,0 +1,10 @@
+package com.hirespace.hirespace_api.repository;
+
+import com.hirespace.hirespace_api.model.Subscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
+    List<Subscription> findByUserId(Long userId);
+}
